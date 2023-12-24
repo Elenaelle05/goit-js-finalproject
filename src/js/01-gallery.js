18d30b045af38678d47d674040eac848172dd635
Elenaelle05 <118297004+Elenaelle05@users.noreply.github.com>
2023-12-24 20:46:34 +0000

    Update 01-gallery.js
    Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
+const galleryList = document.querySelector('.gallery')
 console.log(galleryItems);
+function renderGallery(items){
+    return items.map(({preview, original, description})=>{
+        return(`
+        <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+            data-source="${original}"
+          />
+        </a>
+      </li>    
+        `)
+    })
+}
+galleryList.insertAdjacentHTML('beforeend',renderGallery(galleryItems))
