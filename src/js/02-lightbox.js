74f62a6075a30b391aa0113caf545d64a8fbbeaa
Elenaelle05 <118297004+Elenaelle05@users.noreply.github.com>
2023-12-26 19:25:24 +0000

    task
    Додали розмiтку

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
-
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+
+function createGallery(items) {
+  return items.map(({ preview, original, description }) => {
+    return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          alt="${description}"
+        />
+      </a>
+    </li>`
+  }).join('');
+}
+galleryContainer.insertAdjacentHTML('beforeend', createGallery(galleryItems));
